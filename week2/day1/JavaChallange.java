7f38e96356d75661b58d8f3bf1acb2800fb0eab2
vidhyaa2024 <vidhyaajanakiraman@gmail.com>
2024-12-24 08:01:22 +0530

    Add files via upload
    Day1- Selenium Locator and X-path

---
+++ week2/day1/JavaChallange.java
@@ -0,0 +1,22 @@
+package week2.day1;
+
+public class JavaChallange 
+{
+
+	
+	public static void main(String[] args) 
+	{
+		
+		String s = "Hello World";
+		s.length();
+		//System.out.println(" Lengh of the String : "+s.length());
+		
+		// Spilt Function 
+		s.split(" ");
+		
+		System.out.println(" Lengh of the Last Word : "+ (s.length()-1) );
+		
+	}
+	}
+
+
